d305dee2c54e191d576080106ac004c35694d00c
Tiago Rocha <tiagoarrocha@gmail.com>
2022-06-27 21:25:47 -0300

    Salvando usuário no faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({ 
   providers: [
@@ -8,4 +11,30 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENTE_SECRET,
     }),
   ],
+  secret: process.env.SIGNING_KEY,
+  callbacks: {
+    async signIn({user, account, profile}){
+      try {
+        console.log(process.env.FAUNADB_KEY);
+        
+        const { email } = user;
+        console.log(email);
+        
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {
+              data: {
+                email
+              }
+            }
+          )
+        )
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
 })
