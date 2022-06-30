816d5e456ad44c95af802af2d1e7867ca47dd1aa
Tiago Rocha <tiagoarrocha@gmail.com>
2022-06-29 22:15:41 -0300

    Criando tratamento para webhooks

--- src/pages/components/SubscribeButton/index.tsx
+++ src/pages/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../../services/api';
+import { getStripeJs } from '../../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubsCribeButtonProps {
@@ -10,13 +12,24 @@ export function SubscribeButton({priceId}: SubsCribeButtonProps) {
 
     const { data: session } = useSession()
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if (!session) {
             signIn('github');
             return;
         }
 
         // criação da checkout session
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            stripe.redirectToCheckout({sessionId});
+        } catch (error) {
+            alert(error.message);
+        }
     }
 
     return (
